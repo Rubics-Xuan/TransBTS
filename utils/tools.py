ab5fa1236abdee5931427bdbf990d31a28383adf
Raymond <56334198+Wenxuan-1119@users.noreply.github.com>
2021-03-06 18:48:30 +0800

    Add code for loss functions

---
+++ utils/tools.py
@@ -0,0 +1,8 @@
+import torch.distributed as dist
+
+
+def all_reduce_tensor(tensor, op=dist.ReduceOp.SUM, world_size=1):
+    tensor = tensor.clone()
+    dist.all_reduce(tensor, op)
+    tensor.div_(world_size)
+    return tensor
