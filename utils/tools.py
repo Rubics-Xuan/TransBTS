3a88f9791af470366fe8828378b3fceca28f0821
Raymond <56334198+Wenxuan-1119@users.noreply.github.com>
2021-03-06 18:37:33 +0800

    Delete utils directory

--- utils/tools.py
+++
@@ -1,8 +0,0 @@
-import torch.distributed as dist
-
-
-def all_reduce_tensor(tensor, op=dist.ReduceOp.SUM, world_size=1):
-    tensor = tensor.clone()
-    dist.all_reduce(tensor, op)
-    tensor.div_(world_size)
-    return tensor
